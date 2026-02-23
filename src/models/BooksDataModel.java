31a1ab2b769e3f8d04f1fba603832d537ecce38d
Ulukman A. <ulukman303@gmail.com>
2026-02-23 15:57:26 +0600

    Добавил Модели

---
+++ src/models/BooksDataModel.java
@@ -0,0 +1,18 @@
+package models;
+
+import java.util.List;
+
+public class BooksDataModel {
+    private List<Book> books;
+
+    public BooksDataModel() {
+        books = List.of(new Book(123, "Harry Potter", "Joan Roaring"),
+                new Book(132, "Simba", "Noel Shrek"),
+                new Book(12, "Elder Ring", "Miyamoto"));
+        books.get(1).takeBook(new Employee(12, "Toronto", "Danil", "Sharpovich"));
+    }
+
+    public List<Book> getBooks() {
+        return books;
+    }
+}
